3d6150b518d959e128652ee3ce6b9ec5d5db2479
TheoNorris <63970056+TheoNorris@users.noreply.github.com>
2024-02-15 16:20:25 +0100

    Added basic styling to the billing form

---
+++ wp-content/themes/mytheme/resources/scripts/checkout.js
@@ -0,0 +1,19 @@
+/*
+document.addEventListener("DOMContentLoaded", function () {
+  var firstName = document.getElementById("billing_first_name_field");
+  var lastName = document.getElementById("billing_last_name_field");
+
+  var wrapper = document.querySelector(
+    ".woocommerce-billing-fields__field-wrapper"
+  );
+
+  var nameWrapper = document.createElement("div");
+
+  nameWrapper.className = "name-wrapper";
+
+  nameWrapper.appendChild(firstName);
+  nameWrapper.appendChild(lastName);
+
+  wrapper.appendChild(nameWrapper);
+});
+*/
