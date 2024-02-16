06bfa43fe3ee149ae7449d251567eaf8bd6e43ba
MrGauws <120020363+MrGauws@users.noreply.github.com>
2024-02-17 00:45:50 +0100

    footer

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -2,15 +2,15 @@
     <section class="footer2">
         <div class="column">
             <h1>Free Delivery</h1>
-            <p>For all oders over $50, consectetur adipim scing elit.</p>
+            <p>For all orders over $50, consectetur adipiscing elit.</p>
         </div>
         <div class="column">
             <h2>90 Days Return</h2>
-            <p>If goods have problems, consectetur adipim scing elit.</p>
+            <p>If goods have problems, consectetur adipiscing elit.</p>
         </div>
         <div class="column">
             <h2>Secure Payment</h2>
-            <p>100% secure payment, consectetur adipim scing elit.</p>
+            <p>100% secure payment, consectetur adipiscing elit.</p>
         </div>
     </section>
 </footer>
@@ -20,12 +20,16 @@
     <section class="container">
         <div class="column">
             <?php
+            // Hämta butiksadress, stad, postnummer och land
             $store_address = get_option('woocommerce_store_address');
-            $store_address .= get_option('woocommerce_store_city');
-            $store_address .= get_option('woocommerce_store_postcode');
-            $store_address .= get_option('woocommerce_store_country');
+            $store_city = get_option('woocommerce_store_city');
+            $store_postcode = get_option('woocommerce_store_postcode');
+            $store_country = get_option('woocommerce_store_country');
             
-            echo esc_html($store_address);
+            // Skriv ut adressen med korrekt formatering
+            echo esc_html($store_address) . '';
+            echo esc_html($store_city) . ',<br>';
+            echo 'FL ' . $store_postcode . ' USA';
             ?>
         </div>
         <div class="column">
@@ -42,23 +46,19 @@
         </div>
         <div class="column">
             <span class="category">Help</span>
-            <p>
-                <?php
-                $store_address = get_option('store_address');
-                $store_phone = get_option('store_phone');
-                $store_email = get_option('store_email');
-
-                if (!empty($store_address)) {
-                    echo '<i class="fas fa-map-marker-alt"></i>    ' . esc_html($store_address) . '<br>';
-                }
-                if (!empty($store_phone)) {
-                    echo '<i class="fas fa-phone"></i>    ' . esc_html($store_phone) . '<br>';
-                }
-                if (!empty($store_email)) {
-                    echo '<i class="far fa-envelope"></i>    ' . esc_html($store_email) . '<br>';
-                }
-                ?>
-            </p>
+            <div class="column-51">
+        
+                <?php 
+                $menu = array(
+                    'theme_location' => 'Footer Menu 1',
+                    'menu_id' => 'Footer Menu 1',
+                    'container' => 'nav',
+                    'container_class' => 'Footer Menu 1'
+                );
+                
+                wp_nav_menu($menu); ?>    
+            <!--   <button class="hamburger">&#9776;</button>      -->
+            </div>
         </div>
         <div class="column">
             <span class="category">Newsletter</span>
@@ -73,10 +73,7 @@
     </div>
 </footer>
 
-
-
-
-    <?php 
+<?php 
 wp_footer();
 ?>
 </body>
