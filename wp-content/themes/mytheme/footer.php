96e398752e13e00248dd0df4208b1f1f45daa125
MrGauws <120020363+MrGauws@users.noreply.github.com>
2024-02-16 12:35:15 +0100

    2nd footer

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -1,29 +1,41 @@
+<footer>
+    <section class="footer2">
+        <div class="column">
+            <h1>Free Delivery</h1>
+            <p>For all oders over $50, consectetur adipim scing elit.</p>
+        </div>
+        <div class="column">
+            <h2>90 Days Return</h2>
+            <p>If goods have problems, consectetur adipim scing elit.</p>
+        </div>
+        <div class="column">
+            <h2>Secure Payment</h2>
+            <p>100% secure payment, consectetur adipim scing elit.</p>
+        </div>
+    </section>
+</footer>
 
+<!-- Din befintliga footer fortsätter här -->
 <footer>
     <section class="container">
-    <div class="column">
- 
-    Sätt in hooks för adress här
-
-
-</div>
-<div class="column">
- <span class="category">Links</span>
- <?php
-                    $menu_about_us = array(
-                        'theme_location' => 'huvudmeny',
-                        'menu_id' => 'footermeny',
-                        'container' => 'nav',
-                        'container_class' => 'menu'
-                    );
-                    wp_nav_menu($menu_about_us);
-                ?>
-
-
-</div>
-<div class="column">
- <span class="category">Help</span>
- <p>
+        <div class="column">
+            Sätt in hooks för adress här
+        </div>
+        <div class="column">
+            <span class="category">Links</span>
+            <?php
+            $menu_about_us = array(
+                'theme_location' => 'huvudmeny',
+                'menu_id' => 'footermeny',
+                'container' => 'nav',
+                'container_class' => 'menu'
+            );
+            wp_nav_menu($menu_about_us);
+            ?>
+        </div>
+        <div class="column">
+            <span class="category">Help</span>
+            <p>
                 <?php
                 $store_address = get_option('store_address');
                 $store_phone = get_option('store_phone');
@@ -40,27 +52,20 @@
                 }
                 ?>
             </p>
-
-
-</div>
-<div class="column">
- <span class="category">Newsletter</span>
- <div class="social-icons">
-      
- <a href="#" target="_blank"><i class="fab fa-facebook social_media_icon"></i></a>
- <a href="#" target="_blank"><i class="fab fa-twitter social_media_icon"></i></a>
- <a href="#" target="_blank"><i class="fab fa-pinterest social_media_icon"></i></a>
-
- <a href="#" target="_blank"><i class="fab fa-linkedin social_media_icon"></i></a>
-
-
         </div>
-</div>
-
-</section>
-<div class="copyright">
-            <p>&copy; <?php echo date("Y"); ?> All rights reserved</p>
+        <div class="column">
+            <span class="category">Newsletter</span>
+            <div class="social-icons">
+                <a href="#" target="_blank"><i class="fab fa-facebook social_media_icon"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-twitter social_media_icon"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-pinterest social_media_icon"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-linkedin social_media_icon"></i></a>
+            </div>
         </div>
+    </section>
+    <div class="copyright">
+        <p>&copy; <?php echo date("Y"); ?> All rights reserved</p>
+    </div>
 </footer>
 
 
