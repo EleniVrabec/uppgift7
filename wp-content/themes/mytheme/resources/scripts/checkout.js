cace76add0c2876ebcb64ad63bee4393dfc3d773
TheoNorris <63970056+TheoNorris@users.noreply.github.com>
2024-02-17 17:44:57 +0100

    styled payment options and submit button, added javascript for changing payment option

--- wp-content/themes/mytheme/resources/scripts/checkout.js
+++ wp-content/themes/mytheme/resources/scripts/checkout.js
@@ -3,4 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   if (billingAddressInput) {
     billingAddressInput.value = ""; // Set placeholder to empty string
   }
+
+  document.addEventListener("click", function (event) {
+    var clickedItem = event.target.closest("li.wc_payment_method");
+    if (clickedItem) {
+      // Reset the color of all labels within all li elements with class wc_payment_method to gray
+      document
+        .querySelectorAll("li.wc_payment_method label")
+        .forEach(function (label) {
+          label.style.color = "gray";
+        });
+
+      // Find the label associated with the clicked item
+      var label = clickedItem.querySelector("label");
+      var input = clickedItem.querySelector("input");
+      if (label && (event.target === label || event.target === input)) {
+        // Change the color of the label to black
+        label.style.color = "black";
+      }
+    }
+  });
 });
