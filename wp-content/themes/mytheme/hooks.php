074e9254c13cf6fcb23449596306c476b78ecd3c
EleniVrabec <eleni.vrabec@gritacademy.se>
2024-02-17 10:51:35 +0100

    ll

--- wp-content/themes/mytheme/hooks.php
+++ wp-content/themes/mytheme/hooks.php
@@ -28,3 +28,27 @@ function customize_order_comments_field($fields) {
 
     return $fields;
 }
+
+//remove the add to cart btn from shop page
+add_action( 'woocommerce_after_shop_loop_item', 'remove_add_to_cart_buttons', 1 );
+
+    function remove_add_to_cart_buttons() {
+      if( is_shop()) { 
+        remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
+      }
+    }
+
+// hook to wrap the showing results and sorting in shop page
+add_action( 'woocommerce_before_shop_loop', 'custom_wrapper_open', 19 );
+    function custom_wrapper_open() {
+        echo '<div class="custom-wrapper">';
+    }
+    
+ add_action( 'woocommerce_before_shop_loop', 'custom_wrapper_close', 31 );
+    function custom_wrapper_close() {
+        echo '</div>';
+    }
+
+// remove the sidebar and search in the shop page
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
