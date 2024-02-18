f0ec3b06872ac0640b50b99d0f27e65e1b766d49
TheoNorris <63970056+TheoNorris@users.noreply.github.com>
2024-02-18 17:47:55 +0100

    removed USP from homepage

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -1,18 +1,20 @@
 <footer>
-    <section class="footer2">
-        <div class="column">
-            <h1>Free Delivery</h1>
-            <p>For all orders over $50, consectetur adipiscing scing elit.</p>
-        </div>
-        <div class="column">
-            <h2>90 Days Return</h2>
-            <p>If goods have problems, consectetur adipiscing scing elit.</p>
-        </div>
-        <div class="column">
-            <h2>Secure Payment</h2>
-            <p>100% secure payment, consectetur adipiscing scing elit.</p>
-        </div>
-    </section>
+    <?php if ( ! is_front_page() ) : ?>
+        <section class="footer2">
+            <div class="column">
+                <h1>Free Delivery</h1>
+                <p>For all orders over $50, consectetur adipiscing scing elit.</p>
+            </div>
+            <div class="column">
+                <h2>90 Days Return</h2>
+                <p>If goods have problems, consectetur adipiscing scing elit.</p>
+            </div>
+            <div class="column">
+                <h2>Secure Payment</h2>
+                <p>100% secure payment, consectetur adipiscing scing elit.</p>
+            </div>
+        </section>
+    <?php endif; ?>
 </footer>
 
 <footer>
