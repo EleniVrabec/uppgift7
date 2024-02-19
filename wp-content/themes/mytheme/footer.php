b99fd3a9dc3dfd503c67a34c77d7e10868c94bcd
TheoNorris <63970056+TheoNorris@users.noreply.github.com>
2024-02-19 19:46:06 +0100

    added vw to each block on home

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -18,64 +18,7 @@
 </footer>
 
 <footer>
-    <section class="container">
-        <div class="footercolumn1">
-            <?php
-            // Hämta butiksadress, stad, postnummer och land
-            $store_address = get_option('woocommerce_store_address');
-            $store_city = get_option('woocommerce_store_city');
-            $store_postcode = get_option('woocommerce_store_postcode');
-            $store_country = get_option('woocommerce_store_country');
-            
-            // Skriv ut adressen med korrekt formatering
-            echo esc_html($store_address) . '';
-            echo esc_html($store_city) . ',<br>';
-            echo 'FL ' . $store_postcode . ' USA';
-            ?>
-        </div>
-        <div class="column">
-            <span class="category">Links</span>
-            <?php
-            $menu_about_us = array(
-                'theme_location' => 'huvudmeny',
-                'menu_id' => 'footermeny',
-                'container' => 'nav',
-                'container_class' => 'menu'
-            );
-            wp_nav_menu($menu_about_us);
-            ?>
-        </div>
-        <div class="column">
-            <span class="category">Help</span>
-            <div class="column-51">
     
-                <?php 
-                $menu = array(
-                    'menu' => 'Footer', 
-                    'menu_id' => 'footermeny',
-                    'container' => 'nav',
-                    'theme_location' => 'Footer Menu 1',
-                    'container_class' => 'Footer_Menu_1' 
-                );
-
-                wp_nav_menu($menu);
-                ?>
-                
-            <!--   <button class="hamburger">&#9776;</button>      -->
-            </div>
-        </div>
-        <div class="column">
-            <span class="category">Newsletter</span>
-            <div class="newsletter-form">
-                <input type="email" placeholder="Enter Your Email Address">
-                <button type="submit">SUBSCRIBE</button>
-            </div>
-        </div>
-    </section>
-    <div class="footer-border"></div>
-    <div class="copyright">
-        <p>&copy; <?php echo date("Y"); ?> All rights reserved</p>
-    </div>
 </footer>
 
 <?php 
