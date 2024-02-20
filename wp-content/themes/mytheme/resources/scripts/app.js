9f0f0fd7fc64d74c1f6332a83790bbaa55817de8
TheoNorris <63970056+TheoNorris@users.noreply.github.com>
2024-02-20 14:37:48 +0100

    Styled variation buttons, added javascript for selected variations styled clear button

--- wp-content/themes/mytheme/resources/scripts/app.js
+++ wp-content/themes/mytheme/resources/scripts/app.js
@@ -1,3 +1,30 @@
 import "./test";
 import "./checkout";
 
+document.addEventListener("DOMContentLoaded", function () {
+  const colorItems = document.querySelectorAll(".color-variable-item");
+
+  // Loop through each li element and add click event listener
+  colorItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      // Remove border from all li elements
+      colorItems.forEach((item) => {
+        item.style.border = "none";
+      });
+      // Add border to the clicked li element
+      this.style.border = "2px solid gray";
+    });
+  });
+
+  const sizeItems = document.querySelectorAll(".button-variable-item");
+
+  sizeItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      sizeItems.forEach((item) => {
+        item.style.border = "none";
+      });
+
+      this.style.border = "1px solid black";
+    });
+  });
+});
