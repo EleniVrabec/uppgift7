4289ad72f218257899420ccb8638a5d8ea6c7380
MrGauws <120020363+MrGauws@users.noreply.github.com>
2024-02-20 16:09:38 +0100

    button color size

--- wp-content/themes/mytheme/resources/scripts/app.js
+++ wp-content/themes/mytheme/resources/scripts/app.js
@@ -3,28 +3,46 @@ import "./checkout";
 
 document.addEventListener("DOMContentLoaded", function () {
   const colorItems = document.querySelectorAll(".color-variable-item");
+  const sizeItems = document.querySelectorAll(".button-variable-item");
 
-  // Loop through each li element and add click event listener
+  
   colorItems.forEach((item) => {
     item.addEventListener("click", function () {
-      // Remove border from all li elements
+      
       colorItems.forEach((item) => {
         item.style.border = "none";
       });
-      // Add border to the clicked li element
+      
       this.style.border = "2px solid gray";
     });
   });
 
-  const sizeItems = document.querySelectorAll(".button-variable-item");
+  
+  function clearBorder(items) {
+    items.forEach((item) => {
+      item.style.border = "none";
+    });
+  }
 
+  
   sizeItems.forEach((item) => {
     item.addEventListener("click", function () {
-      sizeItems.forEach((item) => {
-        item.style.border = "none";
-      });
-
+      
+      clearBorder(sizeItems);
+      
       this.style.border = "1px solid black";
+    });
+  });
+
+  
+  sizeItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      
+      sizeItems.forEach((item) => {
+        item.style.backgroundColor = "";
+      });
+      
+      this.style.backgroundColor = "#FBEBB5";
     });
   });
 });
